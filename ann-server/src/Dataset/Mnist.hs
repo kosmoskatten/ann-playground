f14a66e37b08dfd69e702b52de1f85f889d7e637
Patrik Sandahl <patrik.sandahl@ericsson.com>
2016-11-03 15:57:41 +0100

    Moving stuff around.

--- ann-server/src/Dataset/Mnist.hs
+++ ann-server/src/Dataset/Mnist.hs
@@ -1,17 +1,30 @@
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Dataset.Mnist
-    ( mnistFromFile
+    ( trainingPipeline
+    , showError
     ) where
 
 import Control.Monad (when)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Error.Class (MonadError)
+import Control.Monad.Trans.Resource (MonadResource)
+import Data.Csv.Conduit
+import Data.Conduit
+import Data.Conduit.Combinators (sourceFileBS)
+
 import Data.Csv
 import Data.ByteString.Char8 as BS
-import Data.ByteString.Lazy.Char8 as LBS
 import qualified Data.Vector as Vec
 import qualified Data.Vector.Storable as SVec
 
-import Dataset.Types (Bundle (..), OutputData)
+-- TODO: Should come from hfann
+type InputData  = SVec.Vector Float
+type OutputData = SVec.Vector Float
+
+newtype Bundle = Bundle (InputData, OutputData)
+    deriving Show
 
 instance FromRecord Bundle where
     parseRecord line
@@ -25,12 +38,21 @@ instance FromRecord Bundle where
 
         | otherwise              = fail "CSV line length error (shall be 785)"
 
-mnistFromFile :: FilePath -> IO (Either String (Vec.Vector Bundle))
-mnistFromFile file = do
-    content <- LBS.readFile file
-    case decode NoHeader content of
-        Right xs -> return $ Right xs
-        Left err -> return $ Left err
+trainingPipeline :: (MonadError CsvParseError m, MonadResource m)
+                 => FilePath -> m()
+trainingPipeline file =
+    (sourceFileBS file =$= fromCsv defaultDecodeOptions NoHeader) $$ sinkBundles
+
+showError :: CsvParseError -> String
+showError _ = "baah"
+
+sinkBundles :: (MonadIO m) => Sink Bundle m ()
+sinkBundles =
+    awaitForever $ \b -> do
+        liftIO $ Prelude.putStrLn "---"
+        liftIO $ Prelude.print b
+        liftIO $ Prelude.putStrLn "---"
+        sinkBundles
 
 parseInput :: Vec.Vector BS.ByteString -> Parser (Vec.Vector Float)
 parseInput = Vec.mapM parseField
--- ann-server/src/Main.hs
+++ ann-server/src/Main.hs
@@ -10,26 +10,10 @@ import Data.Csv.Conduit
 import Data.Conduit
 import Data.Conduit.Combinators
 
-import Dataset.Types
 import Dataset.Mnist
 
 main :: IO ()
 main = do
     --runConduitRes $ (sourceFileBS "datasets/mnist_train.csv") $$ (sinkFileBS "/tmp/csvcopy.csv")
-    res <- runEitherT $ bimapEitherT showError id $ runResourceT trainingPipeline
+    res <- runEitherT $ bimapEitherT showError id $ runResourceT (trainingPipeline "datasets/mnist_mini.csv")
     either putStrLn return res
-
-trainingPipeline :: (MonadError CsvParseError m, MonadResource m) => m ()
-trainingPipeline =
-    (sourceFileBS "datasets/mnist_mini.csv" =$= fromCsv defaultDecodeOptions NoHeader) $$ sinkBundles
-
-showError :: CsvParseError -> String
-showError _ = "baah"
-
-sinkBundles :: (MonadIO m) => Sink Bundle m ()
-sinkBundles =
-    awaitForever $ \b -> do
-        liftIO $ putStrLn "---"
-        liftIO $ Prelude.print b
-        liftIO $ putStrLn "---"
-        sinkBundles
